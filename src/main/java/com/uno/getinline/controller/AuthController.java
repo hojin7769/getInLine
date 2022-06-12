3f9b1695cc0d151b284d681e7a564c6be57efc8a
hojin <sungsill150@gmail.com>
2022-06-12 15:21:59 +0900

    일단 저장
    일단 저장하는 컨트롤러

---
+++ src/main/java/com/uno/getinline/controller/AuthController.java
@@ -0,0 +1,13 @@
+package com.uno.getinline.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AuthController {
+
+    @GetMapping("/")
+    public String root(){
+        return "index";
+    }
+}
