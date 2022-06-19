9bc3a2fe50df67901335bd8bb11b98f2a860f7f1
hojin <sungsill150@gmail.com>
2022-06-19 14:58:39 +0900

    Add #2 - view controllers
    5가지 뷰 컨트롤러의 작성

--- src/main/java/com/uno/getinline/controller/AuthController.java
+++ src/main/java/com/uno/getinline/controller/AuthController.java
@@ -6,8 +6,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class AuthController {
 
-    @GetMapping("/")
-    public String root(){
-        return "index";
+    @GetMapping("/login")
+    public String login(){
+        return "auth/login";
+
     }
+
+    @GetMapping("/sign-up")
+    public String signUp(){
+        return "auth/sign-up";
+    }
+
 }
