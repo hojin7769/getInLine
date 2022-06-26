2fb82d007062c70ee0ca3199754611bdf623600e
hojin <sungsill150@gmail.com>
2022-06-26 21:13:37 +0900

    test add testing part
    테스트를 위한 테스트 컨트롤러 작성

--- src/test/java/com/uno/getinline/controller/BaseControllerTest.java
+++ src/test/java/com/uno/getinline/controller/BaseControllerTest.java
@@ -1,39 +1,38 @@
 package com.uno.getinline.controller;
 
-import org.hamcrest.CoreMatchers;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.hamcrest.CoreMatchers.containsString;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-@AutoConfigureMockMvc
-@SpringBootTest
+@WebMvcTest(BaseController.class)
 class BaseControllerTest {
-    @Autowired
-    private MockMvc mvc;
 
+    private final MockMvc mvc;
+
+    public BaseControllerTest(@Autowired MockMvc mvc) {
+        this.mvc = mvc;
+    }
+
+    @DisplayName("[view][GET] 기본 페이지 요청")
     @Test
-    void root() throws Exception {
-        //Given
+    void givenNothing_whenRequestingRootPage_thenReturnsIndexPage() throws Exception {
+        // Given
 
-
-        //When
+        // When & Then
         mvc.perform(get("/"))
                 .andExpect(status().isOk())
-                .andExpect(MockMvcResultMatchers.content().contentTypeCompatibleWith(MediaType.TEXT_HTML ))
-                .andExpect(content().string(CoreMatchers.containsString("This is default page")))
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_HTML))
+                .andExpect(content().string(containsString("This is default page.")))
                 .andExpect(view().name("index"))
                 .andDo(print());
-
-
-        //then
     }
+
 }
